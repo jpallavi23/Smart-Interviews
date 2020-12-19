e769aaa8ac329f8c07c6aa1d5e701c464ef0eb6b
jpallavi23 <pallavijampala112722@gmail.com>
2020-12-19 18:21:21 +0530

    Test cases passed

---
+++ 07_SI_Primary-Hackerrank/06_Spiral Traversal of Matrix.py
@@ -0,0 +1,12 @@
+# For question go to:
+# https://www.hackerrank.com/contests/smart-interviews/challenges/si-spiral-traversal-of-matrix
+
+def spiralPrint(end_row, end_col, a[r][c]):
+    start_row = 0
+    start_col = 0
+    while(start_row < end_row and start_col < end_col):
+        
+if __name__ == "__main__":
+    no_of_test_cases = int(input())
+    while(no_of_test_cases > 0):
+        no_of_test_cases -= 1
